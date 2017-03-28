8765e96f6f2f48cd4e6f083a1f669c96d960212b
Henrik <henrik@akesson.mobi>
2017-03-28 18:48:39 +0200

    Parameterized the script

--- build.sh
+++ build.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
+ORG=mxcl
+REPO=PromiseKit
+TAG=4.1.8
 
-mkdir checkout
-cd checkout
-git clone https://github.com/mxcl/PromiseKit
-cd PromiseKit
+ORG=$1
+REPO=$2
+TAG=$3
+
+rm -rf Checkout
+rm *.framework.zip
+
+if [ -z $ORG ]; then
+  echo "Missing org. Usage: ./build.sh <org> <repo> <tag>";
+  exit
+fi
+
+if [ -z $REPO ]; then
+  echo "Missing repo. Usage: ./build.sh <org> <repo> <tag>";
+  exit
+fi
+
+mkdir Checkout
+cd Checkout
+git clone https://github.com/$ORG/$REPO
+cd $REPO
+
+if [ -z $TAG ]; then
+  echo "no tag found: building for master";
+else
+  echo "building tag: $TAG";
+  git checkout $TAG
+fi
+
 carthage build --no-skip-current --platform iOS
-#produce PromiseKit.framework.zip
-carthage archive PromiseKit
-mv PromiseKit.framework.zip ../..
+carthage archive $REPO
+mv $REPO.framework.zip ../..
