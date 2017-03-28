4311fdee85ab36a932f0b2aed9d7c579cfdef565
Henrik <henrik@akesson.mobi>
2017-03-28 20:08:30 +0200

    Further improvements

--- build.sh
+++ build.sh
@@ -3,35 +3,51 @@ ORG=mxcl
 REPO=PromiseKit
 TAG=4.1.8
 
-ORG=$1
-REPO=$2
+ORG=$(echo $1 | cut -f1 -d/)
+REPO=$(echo $1 | cut -f2 -d/)
+ARCHIVES=${2//,/ }
 TAG=$3
 
 rm -rf Checkout
-rm *.framework.zip
+rm -rf Archives
 
 if [ -z $ORG ]; then
-  echo "Missing org. Usage: ./build.sh <org> <repo> <tag>";
+  echo "Missing org. Usage: ./build.sh <org>/<repo> <frameworks,to,archive> [tag]";
   exit
 fi
 
 if [ -z $REPO ]; then
-  echo "Missing repo. Usage: ./build.sh <org> <repo> <tag>";
+  echo "Missing repo. Usage: ./build.sh <org>/<repo> <frameworks,to,archive> [tag]";
   exit
 fi
 
+if [ -z $2 ]; then
+  echo "Archives not specified, using the repo name for carthage archive";
+  ARCHIVES=$REPO
+fi
+
 mkdir Checkout
+mkdir Archives
+
 cd Checkout
-git clone https://github.com/$ORG/$REPO
+git clone https://github.com/$ORG/$REPO || exit 1
 cd $REPO
 
 if [ -z $TAG ]; then
-  echo "no tag found: building for master";
+  if [ -z $TRAVIS_TAG ]; then
+    echo "No tag found: building for master";
+    TAG="master"
+  else
+    TAG=$TRAVIS_TAG
+    echo "Building TRAVIS_TAG: $TAG";
+  fi
 else
-  echo "building tag: $TAG";
-  git checkout $TAG
+  echo "Building tag: $TAG";
 fi
 
-carthage build --no-skip-current --platform iOS
-carthage archive $REPO
-mv $REPO.framework.zip ../..
+git checkout $TAG || { echo "The tag for this repo must match the tag of the mirrored repo."; exit 1; }
+
+carthage build --no-skip-current --platform iOS || exit 1
+echo "Running carthage archive for: $ARCHIVES"
+carthage archive $ARCHIVES || exit 1
+mv *.framework.zip ../../Archives
